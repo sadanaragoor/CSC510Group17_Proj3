3ea76847a82729bb8c5f05afefdfc0494f009cce
Sai Mahathi Suryadevara <s.mahathi7337@gmail.com>
2025-12-07 04:09:59 -0500

    fix: Remove unused import from profile tests conftest

--- proj2/stackshack/tests/profileTests/conftest.py
+++ proj2/stackshack/tests/profileTests/conftest.py
@@ -12,7 +12,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.
 from app import create_app
 from database.db import db
 from models.user import User
-from werkzeug.security import generate_password_hash
 
 
 @pytest.fixture(scope="function")
